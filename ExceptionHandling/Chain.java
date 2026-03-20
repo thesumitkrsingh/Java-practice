b23899da15306c6113ccb12ec6133e05dd892c5b
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-03-20 22:02:23 +0530

    multiple-catch

---
+++ ExceptionHandling/Chain.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+
+public class Chain {
+    public static void main(String[] args){
+       try{
+           divide();
+       } catch (Exception e) {
+           throw new ArithmeticException("Calculation galat hai"+e);
+       }
+    }
+    static void divide(){
+        int a=4;
+        int b=0;
+        int divide=a/b;
+    }
+}
+
