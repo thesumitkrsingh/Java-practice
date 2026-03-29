749a84d9caf6370b68f2f8d8d39362eaa0f86c0f
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-03-29 21:32:02 +0530

    20/03/2026

--- collection/MostRepeatedElement.java
+++ collection/MostRepeatedElement.java
@@ -7,7 +7,18 @@ public class MostRepeatedElement {
         repeated.put(1,1);
         repeated.put(2,2);
         repeated.put(3,1);
-        System.out.println(repeated);
+        //System.out.println(repeated);
+        int maxcount=0;
+        int mostrepeated=-1;
+        for(Map.Entry<Integer,Integer >entry:repeated.entrySet()){
+           if(entry.getValue()> maxcount){
+               maxcount= entry.getValue();
+               mostrepeated=entry.getKey();
+           }
+
+
+        }
+        System.out.println(mostrepeated);
 
 
 
