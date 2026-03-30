77faa4e487e1cd24af1a22aa9c398669b43a9ebd
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-03-30 19:36:12 +0530

    30/03/2026

---
+++ collection/ArrayListIterator.java
@@ -0,0 +1,24 @@
+package collection;
+import java.util.*;
+//Use Iterator to safely remove elements from ArrayList while iterating
+public class ArrayListIterator {
+    public static void main(String[] args){
+        List<String> list=new ArrayList<>();
+        list.add("Sumit");
+        list.add("Sachin");
+        list.add("Karan");
+        Iterator<String> i= list.iterator();
+        while(i.hasNext()){
+            String crnt=i.next();
+            if(crnt.equals("Karan")){
+               i.remove();
+            }
+
+
+        }
+        System.out.println(list);
+
+
+
+    }
+}
