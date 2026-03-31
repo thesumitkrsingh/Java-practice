b125d9bad3233b13c99954fd337842118f90aab8
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-03-31 21:10:52 +0530

    31/03/2026

---
+++ collection/GroupWordByThierFirstLater.java
@@ -0,0 +1,19 @@
+package collection;
+import java.util.*;
+//Group words by their first letter using HashMap<Character, List<String>>
+public class GroupWordByThierFirstLater {
+    public static void main(String[] args){
+        HashMap<Character,List<String>> list= new HashMap<>();
+        List<String> alist=new ArrayList<>();
+        alist.add("Aman");
+        alist.add("Sumit");
+        alist.add("Himanshu");
+       list.put('A',alist);
+       list.put('S',alist);
+       list.put('H',alist);
+        System.out.println(list);
+
+
+
+    }
+}
