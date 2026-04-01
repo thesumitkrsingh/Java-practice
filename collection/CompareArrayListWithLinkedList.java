44ed4baf576c8b293bf85cf84fad9066858ee110
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-02 02:13:29 +0530

    2/04/2026

---
+++ collection/CompareArrayListWithLinkedList.java
@@ -0,0 +1,11 @@
+package collection;
+import java.util.*;
+//Compare ArrayList vs LinkedList — write code showing where each is faster
+public class CompareArrayListWithLinkedList {
+    public static void main(String[] args){
+        List<Integer> list=new ArrayList<>();
+        List<Integer> alist=new LinkedList<>();
+       list.add(2);
+       alist.add(3);
+    }
+}
