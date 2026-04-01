f11987ed3e575bbb65d7f7728fd975491b8dce6b
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-02 02:35:59 +0530

    2/04/2026

---
+++ String/ReverseString.java
@@ -0,0 +1,17 @@
+package String;
+import java.util.*;
+public class ReverseString {
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter a String to reverse");
+        String str=scn.nextLine();
+
+        for(int i=str.length()-1;i>=0;i--){
+
+            System.out.print(str.charAt(i));
+
+        }
+
+
+    }
+}
