e45374242be7489bad4ed9021a3902c691c7e03e
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-05 03:18:26 +0530

    5/04/2026

---
+++ String/AnagramsCheck.java
@@ -0,0 +1,25 @@
+package String;
+import java.util.*;
+//Check if two strings are anagrams of each other
+public class AnagramsCheck {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter First String");
+        String input = scn.nextLine();
+        System.out.println("Enter Second String");
+        String aInput = scn.nextLine();
+        if(input.length()!=aInput.length()){
+            System.out.println("Nota Anagram [length not match");
+            return;
+        }
+        char[] s1=input.toCharArray();
+        char[] s2=aInput.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        if(Arrays.equals(s1,s2)){
+            System.out.println("Its Anagram");
+        }else{
+            System.out.println("Not a Anagram");
+        }
+    }
+}
