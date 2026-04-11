34274ec143b9b3a196c3fe8abf07797376d8468b
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-11 23:36:22 +0530

    11/04/2026

---
+++ String/PalidromeCheck.java
@@ -0,0 +1,8 @@
+package String;
+import java.util.*;
+//Check if a string is a palindrome (ignore case, spaces)
+public class PalidromeCheck {
+    public static void main(String[] args){
+
+    }
+}
