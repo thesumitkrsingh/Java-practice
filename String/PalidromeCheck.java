7b121d243204d43b644b3636ac0bdc16dd01daa0
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-12 11:37:20 +0530

    PalidromeCheck-String MAnipulation

--- String/PalidromeCheck.java
+++ String/PalidromeCheck.java
@@ -1,8 +1,22 @@
 package String;
+import java.sql.SQLOutput;
 import java.util.*;
 //Check if a string is a palindrome (ignore case, spaces)
 public class PalidromeCheck {
     public static void main(String[] args){
+        String input="madam";
+        int left=0;
+        int right=input.length()-1;
+        while(left<right){
+            if(input.charAt(left)!=input.charAt(right )){
+                System.out.println("Its Not Palidrome");
+                return;
+            }
+
+            left++;
+            right--;
+        }
+        System.out.println("Its  A palidrome");
 
     }
 }
