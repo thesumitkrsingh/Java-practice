d3554a9e694e632697b0553b512bb5c542023195
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-13 23:27:50 +0530

    String MAnipulation

---
+++ String/CompareStringVsStringBuilder.java
@@ -0,0 +1,8 @@
+package String;
+import java.util.*;
+public class CompareStringVsStringBuilder {
+    public static void main(String[] args){
+        
+    }
+
+}
---
+++ String/CompressString.java
@@ -0,0 +1,10 @@
+package String;
+import java.util.*;
+public class CompressString {
+    public static void main(String[] args){
+        StringBuilder sb=new StringBuilder("suumiit");
+        for(int i=0;i<sb.length();i++){
+            System.out.println(sb);
+        }
+    }
+}
