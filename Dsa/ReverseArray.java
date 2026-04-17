3b0c62843768560b819660b0b48148c22e1c710a
Thesumitkrsingh <textsumitsingh@gmail.com>
2026-04-17 21:32:18 +0530

    Dsa

---
+++ Dsa/ReverseArray.java
@@ -0,0 +1,13 @@
+package Dsa;
+
+public class ReverseArray {
+public static void main(String[] args){
+    int[] nums={1,2,3,4,5};
+
+    for(int i = nums.length - 1; i >= 0; i--){
+        System.out.print(nums[i]+ " ");
+    }
+
+
+}
+}
